8dbc36c48a618e06986f619cffaef4d86131ad19
evyoliveira <cristinioliveira.eo@gmail.com>
2023-07-23 20:29:23 -0300

    Primitives and custom types

---
+++ 3-TypeCreation/main.go
@@ -0,0 +1,20 @@
+package main
+
+const a = "Hello, World!"
+
+type ID int
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Evelyn"
+	e float64 = 1.2
+	f ID      = 1
+)
+
+func main() {
+	println(f)
+}
+
+func x() {
+}
