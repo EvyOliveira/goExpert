5dffe240ecc2d39d4f78489e825fc1fcc22731b7
evyoliveira <cristinioliveira.eo@gmail.com>
2023-08-22 21:21:53 -0300

    Scrolling maps

--- 7-Maps/main.go
+++ 7-Maps/main.go
@@ -4,9 +4,19 @@ import "fmt"
 
 func main() {
 	salarios := map[string]int{"Evelyn": 5000, "João": 6000, "Maria": 3500}
-	fmt.Println(salarios["Evelyn"])
 	delete(salarios, "Evelyn")
 
 	salarios["Evy"] = 3000
-	fmt.Println(salarios["Evy"])
+
+	//sal := make(map[string]int)
+	//sal1 := map[string]int{}
+	//sal1["Evelyn"] = 10000
+
+	for nome, salario := range salarios {
+		fmt.Printf("O salario de %s é %d\n", nome, salario)
+	}
+
+	for _, salario := range salarios {
+		fmt.Printf("O salario é de %d\n", salario)
+	}
 }
