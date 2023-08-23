e61c6af37581414ddae99cd501ad368b2ba22e9c
evyoliveira <cristinioliveira.eo@gmail.com>
2023-08-22 21:49:28 -0300

    Add function with more than one return

--- 8-Functions/main.go
+++ 8-Functions/main.go
@@ -1,14 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	fmt.Println(sum(51, 2))
+	valor, err := sum(50, 10)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(valor)
+	//fmt.Println(sum(51, 2))
 }
 
-func sum(a, b int) (int, bool) {
+func sum(a, b int) (int, error) {
 	if a+b >= 50 {
-		return a + b, true
+		return 0, errors.New("A soma é maior que 50")
 	}
-	return a + b, false
+	return a + b, nil
 }
