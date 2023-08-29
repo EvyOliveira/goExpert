9cd561db817ddedf1565417cc1c34ec606605878
evyoliveira <cristinioliveira.eo@gmail.com>
2023-08-28 22:09:29 -0300

    Add struct composition

--- 11-Structs/main.go
+++ 11-Structs/main.go
@@ -2,10 +2,25 @@ package main
 
 import "fmt"
 
+type Endereco struct {
+	Logradouro string
+	Numero     int
+	Cidade     string
+	Estado     string
+}
 type Cliente struct {
 	Nome  string
 	Idade int
 	Ativo bool
+	Endereco
+	End Endereco
+}
+
+type Usuario struct {
+	Nome  string
+	Idade int
+	Ativo bool
+	End   Endereco
 }
 
 func main() {
@@ -14,6 +29,11 @@ func main() {
 		Idade: 30,
 		Ativo: true,
 	}
+
 	evelyn.Ativo = false
+	evelyn.Cidade = "São Paulo"
+	evelyn.Endereco.Cidade = "São Paulo"
+	evelyn.End.Logradouro = "Rua I"
+
 	fmt.Printf("Nome: %s, Idade: %d, Ativo: %t", evelyn.Nome, evelyn.Idade, evelyn.Ativo)
 }
