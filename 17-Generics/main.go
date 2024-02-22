65e7945f8cc9f9005fe60d2b1131ffc599b39d5c
evyoliveira <cristinioliveira.eo@gmail.com>
2024-02-22 15:24:55 -0300

    Add generic integer and float sum

--- 17-Generics/main.go
+++ 17-Generics/main.go
@@ -1,7 +1,15 @@
 package main
 
-func Soma(m map[string]int) int {
+func SomaInteiro(m map[string]int) int {
 	var soma int
+	for _, v := range m {
+		soma += v
+	}
+	return soma
+}
+
+func SomaFloat(m map[string]float64) float64 {
+	var soma float64
 	for _, v := range m {
 		soma += v
 	}
@@ -10,5 +18,7 @@ func Soma(m map[string]int) int {
 
 func main() {
 	m := map[string]int{"Evelyn": 10000, "Stephen": 3000, "Bob": 4000}
-	println(Soma(m))
+	m2 := map[string]float64{"Evelyn": 10000.30, "Stephen": 3000.5, "Bob": 4000.0}
+	println(SomaInteiro(m))
+	println(SomaFloat(m2))
 }
