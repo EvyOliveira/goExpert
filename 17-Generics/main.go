560a8f1007502eea3e8c6bd6a7438304aff400b2
evyoliveira <cristinioliveira.eo@gmail.com>
2024-02-22 16:07:18 -0300

    Refactor generic integer and float sum

--- 17-Generics/main.go
+++ 17-Generics/main.go
@@ -1,15 +1,12 @@
 package main
 
-func SomaInteiro(m map[string]int) int {
-	var soma int
-	for _, v := range m {
-		soma += v
-	}
-	return soma
+type MyNumber int
+type Number interface {
+	~int | float64
 }
 
-func SomaFloat(m map[string]float64) float64 {
-	var soma float64
+func Soma[T Number](m map[string]T) T {
+	var soma T
 	for _, v := range m {
 		soma += v
 	}
@@ -19,6 +16,9 @@ func SomaFloat(m map[string]float64) float64 {
 func main() {
 	m := map[string]int{"Evelyn": 10000, "Stephen": 3000, "Bob": 4000}
 	m2 := map[string]float64{"Evelyn": 10000.30, "Stephen": 3000.5, "Bob": 4000.0}
-	println(SomaInteiro(m))
-	println(SomaFloat(m2))
+	m3 := map[string]MyNumber{"Evelyn": 10000, "Stephen": 3000, "Bob": 4000}
+
+	println(Soma(m))
+	println(Soma(m2))
+	println(Soma(m3))
 }
