b5ab18c240c5abe7ebf4074e8a00533e1a0b686e
evyoliveira <cristinioliveira.eo@gmail.com>
2024-03-19 22:12:22 -0300

    Add different multiplexers for endpoints

--- 26-ServerMux/main.go
+++ 26-ServerMux/main.go
@@ -8,6 +8,12 @@ func main() {
 	mux.HandleFunc("/", HomeHandler)
 
 	http.ListenAndServe(":8080", mux)
+
+	mux2 := http.NewServeMux()
+	mux2.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Evy!"))
+	})
+	http.ListenAndServe(":8081", mux2)
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
