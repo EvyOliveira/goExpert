b7d76895829d2a67ea633af8fdf137a39e4ff97d
evyoliveira <cristinioliveira.eo@gmail.com>
2024-04-14 23:01:34 -0300

    Add mapping functions to templates

---
+++ 28-Templates/28.6-TemplateFunctions/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"os"
+	"strings"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func ToUpper(s string) string {
+	return strings.ToUpper(s)
+}
+
+func main() {
+	templates := []string{
+		"header.html",
+		"content.html",
+		"footer.html",
+	}
+
+	t := template.New("content.html")
+	t.Funcs(template.FuncMap{"ToUpper": strings.ToUpper})
+	t = template.Must(t.ParseFiles(templates...))
+
+	err := t.Execute(os.Stdout, Cursos{
+		{"Golang", 40},
+		{"Python", 30},
+		{"Java", 50},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
