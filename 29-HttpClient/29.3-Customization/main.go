10465052d52a4e2eaba8d79bef1a454932fc9016
evyoliveira <cristinioliveira.eo@gmail.com>
2024-05-19 19:34:29 -0300

    Add request customization

---
+++ 29-HttpClient/29.3-Customization/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	c := http.Client{}
+	req, err := http.NewRequest("GET", "http://google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+	resp, err := c.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+}
