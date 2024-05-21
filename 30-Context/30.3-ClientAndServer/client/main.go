a2bf3231277577c81a7653e203d98cdd24b58f09
evyoliveira <cristinioliveira.eo@gmail.com>
2024-05-21 11:50:52 -0300

    Add client-side context

--- 30-Context/30.3-ClientAndServer/client/main.go
+++ 30-Context/30.3-ClientAndServer/client/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080", nil)
