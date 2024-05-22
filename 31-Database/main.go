25f2f775f7563df839817fc60cb2431c1f438182
evyoliveira <cristinioliveira.eo@gmail.com>
2024-05-21 22:45:15 -0300

    Add select all itens function

--- 31-Database/main.go
+++ 31-Database/main.go
@@ -41,11 +41,20 @@ func main() {
 		panic(err)
 	}
 
-	p, err := selectProduct(db, product.ID)
+	//p, err := selectProduct(db, product.ID)
+	//if err != nil {
+	//panic(err)
+	//}
+	//fmt.Printf("Product: %v, possui o preço de %.2f", p.Name, p.Price)
+
+	products, err := selectAllProducts(db)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Product: %v, possui o preço de %.2f", p.Name, p.Price)
+
+	for _, p := range products {
+		fmt.Printf("Product %v, possui o preço de %.2f\n", p.Name, p.Price)
+	}
 }
 
 func insertProduct(db *sql.DB, product *Product) error {
@@ -86,4 +95,23 @@ func selectProduct(db *sql.DB, id string) (*Product, error) {
 		return nil, err
 	}
 	return &p, nil
+}
+
+func selectAllProducts(db *sql.DB) ([]Product, error) {
+	rows, err := db.Query("select id, name, price from products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []Product
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return products, nil
 }
