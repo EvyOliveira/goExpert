6386c39377df1ca3c62876f00261e7ae55297e35
evyoliveira <cristinioliveira.eo@gmail.com>
2024-05-21 22:06:18 -0300

    Add update function

--- 31-Database/main.go
+++ 31-Database/main.go
@@ -22,13 +22,18 @@ func NewProduct(name string, price float64) *Product {
 }
 
 func main() {
-	db, err := sql.Open("mysql", "roo:root@tcp(localhost:3306)/goexpert")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 	product := NewProduct("Notebook", 1899.90)
 	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+	product.Price = 100.00
+	err = updateProduct(db, product)
 	if err != nil {
 		panic(err)
 	}
@@ -41,6 +46,19 @@ func insertProduct(db *sql.DB, product *Product) error {
 	}
 	defer stmt.Close()
 	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
 	if err != nil {
 		return err
 	}
