95ef5843e657a3ae58c455df75333f2ebc386dce
evyoliveira <cristinioliveira.eo@gmail.com>
2024-05-26 14:59:04 -0300

    Add CRUD with gORM

---
+++ 31-Database/31.2-CRUDWithORM/go.mod
@@ -0,0 +1,14 @@
+module github.com/EvyOliveira/goExpert/31
+
+go 1.22.2
+
+require (
+	gorm.io/driver/mysql v1.5.6
+	gorm.io/gorm v1.25.10
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
---
+++ 31-Database/31.2-CRUDWithORM/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID    int `gorm:"primaryKey"`
+	Name  string
+	Price float64
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{})
+}
