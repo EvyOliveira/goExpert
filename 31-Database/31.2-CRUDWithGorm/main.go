e21523a55c9e6f69d28a375d157b7dbfa8bb2aaa
evyoliveira <cristinioliveira.eo@gmail.com>
2024-06-01 11:03:48 -0300

    Fix DSN time parsing

--- 31-Database/31.2-CRUDWithGorm/main.go
+++ 31-Database/31.2-CRUDWithGorm/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,10 +9,11 @@ type Product struct {
 	ID    string `gorm:"primaryKey"`
 	Name  string
 	Price float64
+	gorm.Model
 }
 
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
@@ -78,13 +77,13 @@ func main() {
 	//}
 
 	// modify first record name and delete the secound record
-	var p Product
-	db.First(&p, 1)
-	p.Name = "New Mouse"
-	db.Save(&p)
+	// var p Product
+	// db.First(&p, 1)
+	// p.Name = "New Mouse"
+	// db.Save(&p)
 
-	var p2 Product
-	db.First(&p2, 1)
-	fmt.Println(p2.Name)
-	db.Delete(&p2)
+	// var p2 Product
+	// db.First(&p2, 1)
+	// fmt.Println(p2.Name)
+	// db.Delete(&p2)
 }
