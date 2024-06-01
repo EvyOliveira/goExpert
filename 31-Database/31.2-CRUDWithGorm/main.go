52ca0109a84bd3f6e0966be7403fbb86672505a0
evyoliveira <cristinioliveira.eo@gmail.com>
2024-06-01 09:53:36 -0300

    Feat select one record and select all

---
+++ 31-Database/31.2-CRUDWithGorm/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID    string `gorm:"primaryKey"`
+	Name  string
+	Price float64
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{})
+
+	// create a record
+	// db.Create(&Product{
+	// 	Name:  "Notebook",
+	// 	Price: 2000.00,
+	// })
+
+	// create batch register
+	// products := []Product{
+	//	{ID: uuid.New().String(), Name: "Mousepad", Price: 3000.00},
+	//	{ID: uuid.New().String(), Name: "Keyboard", Price: 3500.00},
+	//	{ID: uuid.New().String(), Name: "Monitor", Price: 4000.00},
+	// }
+	// db.Create(&products)
+
+	// select one record
+	// var product Product
+	// db.First(&product, 2)
+	// fmt.Println(product)
+	// db.First(&product, "name = ?", "Mouse")
+	//fmt.Println(product)
+
+	// select all
+	var products []Product
+	db.Find(&products)
+	for _, product := range products {
+		fmt.Println(product)
+	}
+}
