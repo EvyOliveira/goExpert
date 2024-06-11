5d3197ee772b36fe9a5466eefdf06915d3ff0f00
evyoliveira <cristinioliveira.eo@gmail.com>
2024-06-11 17:50:16 -0300

    Add go routine with sequential tasks

---
+++ 40-GoRoutines/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(name string) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d: Task %s is running\n", i, name)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	task("A")
+	task("B")
+}
