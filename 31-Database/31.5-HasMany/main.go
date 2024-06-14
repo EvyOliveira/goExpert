db403343bec2b78f2bded40077af9593f008afd9
evyoliveira <cristinioliveira.eo@gmail.com>
2024-06-13 22:48:14 -0300

    Add serial number struct applying has many

---
+++ 31-Database/31.5-HasMany/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID           string `gorm:"primaryKey"`
+	Name         string
+	Price        float64
+	CategoryID   int
+	Category     Category
+	SerialNumber SerialNumber
+	gorm.Model
+}
+
+type Category struct {
+	ID       int `gorm:"primaryKey"`
+	Name     string
+	Products []Product
+}
+
+type SerialNumber struct {
+	ID        int `gorm:"primaryKey"`
+	Number    string
+	ProductID int
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{}, &Category{}, &SerialNumber{})
+
+	category := Category{Name: "Cozinha"}
+	db.Create(&category)
+
+	// create category
+	// category := Category{Name: "Eletronicos"}
+	// db.Create(&category)
+
+	// create product
+	db.Create(&Product{
+		Name:       "Panela",
+		Price:      99.00,
+		CategoryID: 1,
+	})
+
+	// create serial number
+	db.Create(&SerialNumber{
+		Number:    "123456",
+		ProductID: 1,
+	})
+
+	var products []Product
+	db.Preload("Category").Preload("SerialNumber").Find(&products)
+	for _, product := range products {
+		fmt.Println(product.Name, product.Category.Name, product.SerialNumber.Number)
+	}
+
+	var categories []Category
+	err = db.Model(&Category{}).Preload("Products.SerialNumber").Find(&categories).Error
+	if err != nil {
+		panic(err)
+	}
+	for _, category := range categories {
+		fmt.Println(category.Name, ":")
+		for _, product := range category.Products {
+			println("-", product.Name, category.Name, "Serial Number:", product.SerialNumber.Number)
+		}
+	}
+}
